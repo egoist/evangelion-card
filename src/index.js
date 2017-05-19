d59788198e977c123b2113886fd2755b072889d8
EGOIST <0x142857@gmail.com>
2017-05-20 01:24:26 +0800

    clean up

--- src/index.js
+++ src/index.js
@@ -3,10 +3,6 @@ import 'to-blob'
 import Vue from 'vue'
 import App from './components/App.vue'
 
-if (process.env.NODE_ENV === 'production') {
-  require('./pwa')
-}
-
 new Vue({
   el: '#app',
   render: h => h(App)
